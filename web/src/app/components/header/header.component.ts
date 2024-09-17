45d9e76e8d5ad7a156dee477ac3e820a5388774b
Guilherme <guilherme-allves@hotmail.com>
2024-09-17 01:29:43 -0300

    web - seta de navegação do home feita

--- web/src/app/components/header/header.component.ts
+++ web/src/app/components/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  arrow = 'assets/images/arrow.png';
 }
